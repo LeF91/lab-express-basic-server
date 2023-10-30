78bb1ecc0dc903f1e2abef959821737db73341f2
Fabien Lacordelle <falaco972@icloud.com>
2023-10-30 12:08:08 +0100

    lab done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require("express");
+const morgan = require("morgan");
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
 
-
+const app = express();
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
@@ -14,12 +14,35 @@
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
 
-
+app.use(express.static("public"));
+app.use(express.json());
+app.use(morgan("dev"));
 
 // ROUTES
 // Start defining your routes here:
+app.get("/", (require, response, next) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
 
+app.get("/blog", (require, response, next) => {
+  response.sendFile(__dirname + "/views/blog.html");
+});
 
+app.get("/api/projects", (require, response, next) => {
+  response.json(projects);
+});
+
+app.get("/api/articles", (req, res, next) => {
+  response.json(articles);
+});
+
+app.get("*", (req, res, next) => {
+  response.status(404).sendFile(__dirname + "views/not-found.html");
+});
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+app.listen(5005, () => {
+  console.log("Server running on http://localhost:5005");
+});
